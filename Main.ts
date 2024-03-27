0ba8de41e4e2bc1d4ce96c1c873d39d2ff29b88f
SufyanKhanDilazak <164017091+SufyanKhanDilazak@users.noreply.github.com>
2024-03-27 14:33:48 +0500

    Add files via upload

---
+++ Main.ts
@@ -0,0 +1,21 @@
+let prompt = require('prompt-sync')();
+let n1 = parseInt(prompt("enter first number "))
+let operator = prompt("enter your operator (+,-,*,/)")
+let n2 = parseInt(prompt("enter second number"))
+if (operator == "+"){
+    console.log(`Answer ${n1} ${operator} ${n2} ${n1 + n2}`)
+}
+else if (operator == "-"){
+    console.log(`Answer ${n1} ${operator} ${n2} ${n1 - n2}`)
+}
+else if (operator == "*"){
+    console.log(`Answer ${n1} ${operator} ${n2} ${n1 * n2}`)
+}
+else if (operator == "/"){
+    console.log(`Answer ${n1} ${operator} ${n2} ${n1 / n2}`)
+}
+else{
+    console.log("Select Correct Numbers")
+}
+
+export {}
